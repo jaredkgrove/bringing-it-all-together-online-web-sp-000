00fe0f0af2d0555078d17a099b57c63c35c109d0
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 23:00:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,7 +20,7 @@ class Dog
 
   def self.new_from_db
 
-  end 
+  end
 
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
