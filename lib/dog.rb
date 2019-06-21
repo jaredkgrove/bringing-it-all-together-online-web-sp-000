cfc45cb270fc46acc5baeac3ebefbc6cb650a066
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 22:39:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,7 +18,12 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * WHERE name = ?, breed = ?", name, breed)
 
+    if !dog.empty?
+      dog.save
+    else
+    end 
   end
 
   def self.find_by_id(id)
